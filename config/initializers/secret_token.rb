33ee21a08b8c2f27fed83583ab5db7fae31cb3e9
Marisol Acuña <mari.maf@gmail.com>
2015-03-21 22:38:30 -0300

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miradaanimal::Application.config.secret_token = '1d69211bd46092673b1d783002189080467ce39f07a5169e246d9a73d8ee4b1f5c577dd38c696a99c62110cd80663a6c33dbf615f8c2534bc29c86b93f2f6299'
