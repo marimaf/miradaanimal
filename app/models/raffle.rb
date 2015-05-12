f71defdcba673f02acebc847e6504769d5ebe698
Marisol Acuña <mari.maf@gmail.com>
2015-05-12 15:32:07 -0400

    se puede editar verificado

--- app/models/raffle.rb
+++ app/models/raffle.rb
@@ -1,5 +1,5 @@
 class Raffle < ActiveRecord::Base
-  attr_accessible :email, :event_number, :number, :receipt
+  attr_accessible :email, :event_number, :number, :receipt, :verified
 
   scope :verificados, where("verified" => true)
   scope :no_verificados, where("verified" => [false, nil])
