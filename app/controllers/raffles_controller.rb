38178de1fe6eababd5c24bc450b6b48c2ec11362
Marisol Acuña <mari.maf@gmail.com>
2015-05-12 15:29:44 -0400

    se revisan que no hayn comprado los numeros

--- app/controllers/raffles_controller.rb
+++ app/controllers/raffles_controller.rb
@@ -40,21 +40,38 @@ class RafflesController < ApplicationController
   # POST /raffles
   # POST /raffles.json
   def create
+    event_number = 1
     numbers = params[:raffle][:number]
     if !numbers.nil? && numbers.count != 0
       numbers.each_with_index do |number, index|
+        if !Raffle.where(:number => number, :event_number => event_number).empty?
+          @raffles = Hash[ Raffle.where(:event_number => event_number).map{|c| [c.number, true] }]
+          @raffle = Raffle.new(params[:raffle])
+          @raffle.number = nil 
+          @limit = 100
+          @dogs = Dog.limit(@limit)
+          @notice = "ALGUIEN YA COMPRO UNO DE LOS NUMEROS QUE ESCOGISTE"
+          render action: "new"
+          return
+        end
+      end
+      numbers.each_with_index do |number, index|
       #if index.odd?
-        @raffle = Raffle.new(params[:raffle])
-        @raffle.event_number = 1
-        @raffle.number = number[0].to_i
-        @raffle.save
+        
+          @raffle = Raffle.new(params[:raffle])
+          @raffle.event_number = event_number
+          @raffle.number = number[0].to_i
+          @raffle.save
+        
+         
+        
       #end
       end
 
       AdminMailer.thanks_raffle(@raffle.email).deliver
       redirect_to @raffle, notice: 'Raffle was successfully created.'
     else
-      @raffles = Hash[ Raffle.where(:event_number => 1).map{|c| [c.number, true] }]
+      @raffles = Hash[ Raffle.where(:event_number => event_number).map{|c| [c.number, true] }]
     @raffle = Raffle.new
     @limit = 100
     @dogs = Dog.limit(@limit)
