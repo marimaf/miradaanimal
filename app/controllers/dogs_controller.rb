882e16fac264c6524e1f7574a2f5973887d08661
Marisol Acuña <mari.maf@gmail.com>
2015-10-24 23:42:35 -0300

    forgot to add dogs controller on last commit

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -3,5 +3,20 @@ class DogsController < InheritedResources::Base
   	@dogs = Dog.where(:adopted => [nil, false]).limit(15)
   end
 
+  #returns the dogs that are not adopted, aka waiting in the shelter to be adopted
+  def waitlist
+
+  	@dogs = Dog.where(:adopted => [nil, false]).limit(25)
+
+  	respond_to do |format|
+      format.html # index.html.erb
+      format.json #{ render json: @dogs }
+    end
+  end
+
+  def waitlistview
+
+  end
+
 end
 
